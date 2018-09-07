bf84edae15944f935ecf1b095bbaa0c9ae4fc5af
Måns <mans.aberg@gmail.com>
2018-09-07 23:00:51 +0200

    Initial commit

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg></svg>
+</template>
+
+<script>
+// Import d3
+import * as d3 from 'd3';
+
+export default {
+  name: 'MainComponent',
+  components: {
+  },
+  data() {
+    return {
+      svg: null,
+      margin: { top: 20, right: 20, bottom: 30, left: 50 },
+    };
+  },
+  mounted() {
+    this.svg = d3.select('svg');
+  },
+  computed: {
+    width() {
+      return this.$el.offsetWidth;
+    },
+    height() {
+      return this.$el.offsetHeight;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang = "scss">
+
+</style>
---
+++ src/d3-components/line-chart.vue
@@ -0,0 +1,17 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+
+export default {
+	name: 'LineChart',
+	props: {
+	},
+	data() {
+		return {
+		};
+	},
+}
+
+</script>
